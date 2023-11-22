c20437195f965cf897be538c05301b5ca4a56e82
CarlosManzano456 <cmanzanos2100@alumno.ipn.mx>
2023-11-21 18:01:27 -0600

    comitsito

---
+++ src/rsa_manita_swya/RSA_MANITA_SWyA.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rsa_manita_swya;
+import GUI.txtCifrar;
+/**
+ *
+ * @author carlo
+ */
+public class RSA_MANITA_SWyA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        txtCifrar cif = new txtCifrar();
+        cif.setVisible(true);
+        cif.setLocationRelativeTo(null);
+    }
+    
+}
